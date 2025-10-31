c0fc43c30670bd20a85a7388ebfd34d78c0f8001
Adam Ratajczak <a3.ratajczak@gmail.com>
2025-11-01 00:21:15 +0100

    punk attack animations

--- entities/player.gd
+++ entities/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const WALKING_SPEED: float = 5000
 const JUMP_SPEED: float = -400.0
 const GRAVITY: float = 981.0
+const LOADING_GUN_DELAY_SEC: float = 0.2
+const GUN_IDLE_DELAY_SEC: float = 0.5
 
 @onready var use_area: Area2D = $UseArea
 @onready var label_press_e_to_use = $PressEToUse
@@ -12,14 +14,32 @@ var can_use_objects: bool = true
 var moving: bool = false
 var jumping: bool = false
 var flipped: bool = false
+var gun_loaded: bool = false
+var gun_loading: bool = false
+var gun_unloading: bool = false
+var last_shot_timestamp: int = 0
 
 func _animation_process(delta: float) -> void:
 	if jumping:
 		$Sprite2D.play("jump")
 	elif moving:
-		$Sprite2D.play("run")
+		if gun_loading:
+			$Sprite2D.play("show_gun_running")
+		elif gun_loaded:
+			$Sprite2D.play("gun_running")
+		elif gun_unloading:
+			$Sprite2D.play("hide_gun_running")
+		else:
+			$Sprite2D.play("running")
 	else:
-		$Sprite2D.play("idle")
+		if gun_loading:
+			$Sprite2D.play("show_gun_idle")
+		elif gun_loaded:
+			$Sprite2D.play("gun_idle")
+		elif gun_unloading:
+			$Sprite2D.play("hide_gun_idle")
+		else:
+			$Sprite2D.play("idle")
 	if abs(velocity.x) > 0.01:
 		flipped = velocity.x < 0
 	$Sprite2D.flip_h = flipped
@@ -45,6 +65,15 @@ func _physics_process(delta: float) -> void:
 
 	velocity.x *= 0.00001 ** delta
 	velocity.y += 981 * delta
+	
+	if gun_loaded and Time.get_ticks_msec() - last_shot_timestamp > GUN_IDLE_DELAY_SEC * 1000.0:
+		gun_loaded = false
+		gun_unloading = true
+		
+		var timer = get_tree().create_timer(LOADING_GUN_DELAY_SEC)
+		timer.timeout.connect(func(): self.gun_unloading = false)
+		
+	
 	move_and_slide()
 
 
@@ -69,11 +98,31 @@ func _unhandled_key_input(event: InputEvent) -> void:
 				var timer = get_tree().create_timer(0.1)
 				timer.timeout.connect(func(): self.can_use_objects = true)
 
-
+func _spawn_shell() -> void:
+	var shell = preload("res://entities/shell.tscn").instantiate()
+	var mouse_pos = get_global_mouse_position()
+	Main.the.current_level().add_child(shell)
+	shell.setup(self, gun_point.global_position, mouse_pos)
+	
+	gun_loading = false
+	gun_unloading = false
+	gun_loaded = true
+	last_shot_timestamp = Time.get_ticks_msec()
+			
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			var shell = preload("res://entities/shell.tscn").instantiate()
+			if jumping:
+				return
+			
 			var mouse_pos = get_global_mouse_position()
-			Main.the.current_level().add_child(shell)
-			shell.setup(self, gun_point.global_position, mouse_pos)
+			if moving and flipped != (gun_point.global_position.x > mouse_pos.x):
+				return
+			flipped = (gun_point.global_position.x > mouse_pos.x)
+			
+			if not gun_loaded:
+				gun_loading = true
+				var timer = get_tree().create_timer(LOADING_GUN_DELAY_SEC)
+				timer.timeout.connect(func(): _spawn_shell())
+			else:
+				_spawn_shell()
