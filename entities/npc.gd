1f78db35a3551b1d8e7240e23e0ff8340c958ebf
Maciej <sppmacd@pm.me>
2025-11-01 14:45:35 +0100

    Implement simple NPC AI

--- entities/npc.gd
+++ entities/npc.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+const WALKING_SPEED: float = 4000
 
 # flipped = facing left
+var dead = false
 var flipped = true
-
+var walking = false
+var following_player = false
 
 func set_flipped(f: bool):
 	flipped = f
@@ -23,6 +26,8 @@ func sight_vector() -> Vector2:
 func damage(v: int):
 	print("DAMAGE ", v)
 	$AnimatedSprite2D.play("death")
+	dead = true
+	velocity = Vector2()
 	await get_tree().create_timer(0.7).timeout
 	var player: Player = Main.the.current_level().player()
 	player.add_key(Keys.lvl2_floor0_upstairs)
@@ -33,18 +38,65 @@ func raytrace_player():
 	var player: Player = Main.the.current_level().player()
 	var collider = $RayCast2D.get_collider()
 	if collider and collider == player:
-		return player.global_position
+		return collider["position"]
 	return null
 
 
-func _physics_process(delta: float) -> void:
+func ai():
 	var p = raytrace_player()
-	print(p)
+	if following_player:
+		if p:
+			$PlayerFollowingCooldown.start()
+			walking = abs(p.x - global_position.x) > 100
+			set_flipped(p.x < global_position.x)
+		else:
+			walking = false
+	else:
+		if p:
+			walking = true
+			following_player = true
+			$PlayerFollowingCooldown.start()
+		else:
+			walking = false
 
+func _physics_process(delta: float) -> void:
+	ai()
+	
+	if walking:
+		velocity.x += -WALKING_SPEED * delta if flipped else WALKING_SPEED * delta
+	velocity.x *= 0.00001 ** delta
+	move_and_slide()
+
+
+func _animation_process() -> void:
+	if dead:
+		return
+	if walking:
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("idle")
+
+
+func _process(_delta: float) -> void:
+	_animation_process()
+	
 
 func _on_timer_timeout() -> void:
-	set_flipped(not flipped)
-	var shell = preload("res://entities/shell.tscn").instantiate()
-	var player = Main.the.current_level().get_node("Player")
-	Main.the.current_level().add_child(shell)
-	shell.setup(self, $GunMarker.global_position, player.global_position - Vector2(0, 50))
+	var p = raytrace_player()
+	if p:
+		var shell = preload("res://entities/shell.tscn").instantiate()
+		Main.the.current_level().add_child(shell)
+		shell.setup(self, $GunMarker.global_position, p)
+
+
+func _on_lookaround_timer_timeout() -> void:
+	if following_player:
+		var p = raytrace_player()
+		if not p:
+			set_flipped(not flipped)
+	else:
+		set_flipped(not flipped)
+
+
+func _on_player_following_cooldown_timeout() -> void:
+	following_player = false
