29f16462803792dcbc4b190cfee656e813b89b73
Maciej <sppmacd@pm.me>
2025-11-01 20:27:03 +0100

    More lvl2 content

--- keys.gd
+++ keys.gd
@@ -2,7 +2,9 @@ class_name Keys
 extends Node
 
 const lvl2_floor0_upstairs: String = "lvl2_floor0_upstairs"
+const lvl2_floor1_upstairs: String = "lvl2_floor1_upstairs"
 
 const KEY_TEXTURES = {
-	Keys.lvl2_floor0_upstairs: "res://assets/keys/lvl2_floor0_upstairs.tres"
+	Keys.lvl2_floor0_upstairs: "res://assets/keys/lvl2_floor0_upstairs.tres",
+	Keys.lvl2_floor1_upstairs: "res://assets/keys/lvl2_floor1_upstairs.tres",
 }
