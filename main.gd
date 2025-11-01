94ed109dbe875192c4da7fea24d91a7907da28b4
Maciej <sppmacd@pm.me>
2025-11-01 11:02:08 +0100

    Add level switching

--- main.gd
+++ main.gd
@@ -2,9 +2,21 @@ class_name Main
 extends Node2D
 
 static var the: Main
+var _current_level: Node2D
 
 func _ready():
 	the = self
+	switch_level(preload("res://levels/Level1.tscn").instantiate())
+	_current_level.add_child(preload("res://entities/player.tscn").instantiate())
 
 func current_level():
-	return $Level2
+	return _current_level
+
+func switch_level(lvl: Node2D):
+	if has_node("Level"):
+		var player: Node2D = get_node("Level").get_node("Player")
+		player.reparent(lvl)
+		remove_child(get_node("Level"))
+	_current_level = lvl
+	lvl.name = "Level"
+	add_child(lvl)
