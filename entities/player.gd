de00d700e4899758e87a300c5c17841658923979
Maciej <sppmacd@pm.me>
2025-11-01 20:01:26 +0100

    Sort usable objects by distance

--- entities/player.gd
+++ entities/player.gd
@@ -95,7 +95,7 @@ func _animation_process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if health <= 0:
 		return
-	
+
 	moving = false
 	if Input.is_key_pressed(KEY_A):
 		velocity.x -= WALKING_SPEED * delta
@@ -103,7 +103,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_D):
 		velocity.x += WALKING_SPEED * delta
 		moving = true
-	
+
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 	else:
@@ -115,15 +115,15 @@ func _physics_process(delta: float) -> void:
 
 	velocity.x *= 0.00001 ** delta
 	velocity.y += 981 * delta
-	
+
 	if gun_loaded and Time.get_ticks_msec() - last_shot_timestamp > GUN_IDLE_DELAY_SEC * 1000.0:
 		gun_loaded = false
 		gun_unloading = true
-		
+
 		var timer = get_tree().create_timer(LOADING_GUN_DELAY_SEC)
 		timer.timeout.connect(func(): self.gun_unloading = false)
-		
-	
+
+
 	move_and_slide()
 
 
@@ -161,7 +161,7 @@ func _process(delta: float) -> void:
 			label_press_e_to_use.visible = false
 	else:
 		label_press_e_to_use.visible = false
-	
+
 	_animation_process(delta)
 
 func _unhandled_key_input(event: InputEvent) -> void:
@@ -170,11 +170,15 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.keycode == KEY_E and event.is_pressed() and can_use_objects:
 			var uo = _find_usable_objects()
+			uo.sort_custom(func(a,b): return global_position.distance_squared_to(a.global_position) < global_position.distance_squared_to(b.global_position))
+			for u in uo:
+				print(u, u.global_position.distance_squared_to(global_position))
+			print(uo)
 			if len(uo) > 0:
 				if uo[0].action_enabled():
 					uo[0].action_use(self)
 					can_use_objects = false
-					
+
 					var timer = get_tree().create_timer(0.1)
 					timer.timeout.connect(func(): self.can_use_objects = true)
 
@@ -190,7 +194,7 @@ func _spawn_shell() -> void:
 	last_shot_timestamp = Time.get_ticks_msec()
 	ammo -= 1
 	ammo_changed.emit()
-			
+
 func _unhandled_input(event: InputEvent) -> void:
 	if health <= 0:
 		return
@@ -202,19 +206,19 @@ func _unhandled_input(event: InputEvent) -> void:
 				if not punching:
 					Punch()
 				return
-			
+
 			var mouse_pos = get_global_mouse_position()
 			if moving and flipped != (gun_point.global_position.x > mouse_pos.x):
 				return
 			flipped = (gun_point.global_position.x > mouse_pos.x)
-			
+
 			if not gun_loaded:
 				gun_loading = true
 				var timer = get_tree().create_timer(LOADING_GUN_DELAY_SEC)
 				timer.timeout.connect(func(): _spawn_shell())
 			else:
 				_spawn_shell()
-				
+
 func Punch() -> void:
 	if health <= 0:
 		return
