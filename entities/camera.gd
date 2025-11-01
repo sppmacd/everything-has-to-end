715cba51867605bd17e48460f327c9d29e1b4888
Maciej <sppmacd@pm.me>
2025-11-01 16:44:48 +0100

    Make camera not activate alarm if destroyed

--- entities/camera.gd
+++ entities/camera.gd
@@ -1,11 +1,16 @@
 extends StaticBody2D
 
+var destroyed: bool = false
+
 func damage(_v: int):
 	$Sprite2D.texture = preload("res://assets/objects/camera_destroyed.tres")
 	$Polygon2D.visible = false
+	destroyed = true
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if destroyed:
+		return
 	print("body ", body, " entered camera area")
 	var cr = Main.the.current_level()
 	if body == cr.player() and cr.has_method(&"launch_alarm"):
