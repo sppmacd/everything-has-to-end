3d214948c5650fa1f60d66781e756cba93e23b72
Adam Ratajczak <a3.ratajczak@gmail.com>
2025-11-02 09:32:59 +0100

    lvl1 finalize

--- keys.gd
+++ keys.gd
@@ -1,10 +1,15 @@
 class_name Keys
 extends Node
 
+const lvl1_garnizon: String = "lvl1_garnizon"
+const lvl1_ministry: String = "lvl1_ministry"
+
 const lvl2_floor0_upstairs: String = "lvl2_floor0_upstairs"
 const lvl2_floor1_upstairs: String = "lvl2_floor1_upstairs"
 
 const KEY_TEXTURES = {
+	Keys.lvl1_garnizon: "res://assets/keys/lvl1_garnizon.tres",
+	Keys.lvl1_ministry: "res://assets/keys/lvl1_ministry.tres",
 	Keys.lvl2_floor0_upstairs: "res://assets/keys/lvl2_floor0_upstairs.tres",
 	Keys.lvl2_floor1_upstairs: "res://assets/keys/lvl2_floor1_upstairs.tres",
 }
